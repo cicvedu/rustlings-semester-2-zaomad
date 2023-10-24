1553014eeec309fd857f29cc85040b98484f6b47
zaomad <104520459+zaomad@users.noreply.github.com>
2023-10-24 10:23:05 +0000

    aaa

--- exercises/move_semantics/move_semantics6.rs
+++ exercises/move_semantics/move_semantics6.rs
@@ -10,7 +10,7 @@
 fn main() {
     let data = "Rust is great!".to_string();
 
-    get_char(data);
+    //get_char(data);
 
     string_uppercase(&data);
 }
@@ -22,7 +22,7 @@ fn get_char(data: String) -> char {
 
 // Should take ownership
 fn string_uppercase(mut data: &String) {
-    data = &data.to_uppercase();
+   // data = &data.to_uppercase();
 
     println!("{}", data);
 }
