b4053ecce99e3e66d2c34b65f781cae5131dcdde
zaomad <104520459+zaomad@users.noreply.github.com>
2023-10-24 07:52:55 +0000

    acb

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,16 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
+
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(weight:i32)->i32{
+    if(weight<=40){
+        weight*2
+    }else{
+        weight
+    }
+}
 
 // Don't modify this function!
 #[test]
